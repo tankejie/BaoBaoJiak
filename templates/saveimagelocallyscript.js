b381573adbd2ddffa2c772850d86f59930e1b803
tankejie <108395229+tankejie@users.noreply.github.com>
2022-07-11 20:22:08 +0800

    Update saveimagelocallyscript.js

--- templates/saveimagelocallyscript.js
+++ templates/saveimagelocallyscript.js
@@ -1,12 +1,13 @@
-function getBase64Image(img) {
-    var canvas = document.createElement("canvas");
-    canvas.width = img.width;
-    canvas.height = img.height;
+const getBase64Image = (img) => {
+  const canvas = document.createElement("canvas");
+  canvas.width = img.width;
+  canvas.height = img.height;
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+  const dataURL = canvas.toDataURL("image/png");
+  return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+};
 
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(img, 0, 0);
-
-    var dataURL = canvas.toDataURL("image/png");
-
-    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-}
+const bannerImage = document.getElementById("bannerImg");
+const imgData = getBase64Image(bannerImage);
+localStorage.setItem("imgData", imgData);
