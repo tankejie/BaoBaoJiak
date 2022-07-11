eab046ff046ec280d0b02de698df8b653c2123da
tankejie <108395229+tankejie@users.noreply.github.com>
2022-07-11 16:15:37 +0800

    Add files via upload

---
+++ templates/saveimagelocallyscript.js
@@ -0,0 +1,18 @@
+var inputImage, imgData
+
+inputImage = document.getElementById('input-image');
+imgData = getBase64Image(inputImage);
+localStorage.setItem("imgData", imgData);
+
+function getBase64Image(img) {
+    var canvas = document.createElement("canvas");
+    canvas.width = img.width;
+    canvas.height = img.height;
+
+    var ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0);
+
+    var dataURL = canvas.toDataURL("image/png");
+
+    return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+}
