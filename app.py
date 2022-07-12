090dbe68f1a3c3dd2863f788f9569224280412a8
tankejie <108395229+tankejie@users.noreply.github.com>
2022-07-12 14:36:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def prediction(filename):
     #Step 5
     return render_template('predict.html', predictions=predictions)
 
-@app.route('/prediction/', methods=['GET', 'POST'])
+@app.route('/prediction', methods=['GET', 'POST'])
 def predict_page():
     if request.method == 'POST':
         file = request.files['file']
